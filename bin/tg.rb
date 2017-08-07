762a40a21666a7f08633b517064ac760ae82f79f
Corey Prophitt <prophittcorey@users.noreply.github.com>
2017-08-06 22:26:04 -0700

    Stubbed the command line tool

---
+++ bin/tg.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# require 'tor-guard'
+require_relative '../lib/tor-guard'
+
+if ARGV.length < 1
+  puts 'Usage: tg [ip-address]'
+  exit 1
+else
+  TorGuard.exit_node?(ARGV.first)
+end
